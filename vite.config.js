bdd5ccc52918d102241b400419aa7bbbf7a3f924
ceciliaostrovsky-dev <ceciliaostrovsky@gmail.com>
2026-05-07 14:08:36 +0200

    Ajuste de padding en header

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/modern-art-gallery",
+  base: "/modern-art-gallery/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        location: "location.html",
+      },
+    },
+  },
   server: {
     open: true,
     port: 5173,
